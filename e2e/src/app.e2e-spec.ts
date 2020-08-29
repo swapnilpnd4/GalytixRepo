b5047a055ae72920ef7b5ffc96e8f0a44a60f7cc
Shashank Pandey <shashankpandey@Shashanks-MacBook-Pro.local>
2020-08-29 18:00:02 +0530

    testcasecompleted

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,85 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('TC_001: Verify that user is at Browse Jobs page', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('galytix-test-exercise app is running!');
+    expect(page.getTitleText()).toEqual('BROWSE JOBS');
+  });
+
+  it('TC_002: Verify all the six jobs link are available on the browse jobs page', () => {
+
+    expect(page.getJobLinkText_SrSoftwareDevGurgaon()).toEqual('Sr. Software Developer');
+    expect(page.getJobLinkText_SrSoftwareDevLondon()).toEqual('Sr. Software Developer');
+
+    expect(page.getJobLinkText_TestingEngGurgaon()).toEqual('Testing Engineer');
+    expect(page.getJobLinkText_TestingEngLondon()).toEqual('Testing Engineer');
+
+    expect(page.getJobLinkText_SrWebDesignerGurgaon()).toEqual('Sr. Web Designer');
+    expect(page.getJobLinkText_SrWebDesignerLondon()).toEqual('Sr. Web Designer');
+  });
+
+  it('TC_003: Verify that user should able to apply for Sr Software developer for Gurgaon Location', () => {
+    page.click_SrSoftwareDevGurgaon();
+    expect(page.getJobDescription_SrSoftwareDevGurgaon()).toEqual('Gurgaon, India');
+    page.click_ApplyNowButton();
+    browser.sleep(2000);
+    expect(page.verifyAttachResumePopUp()).toEqual('Attach Resume');
+  });
+
+  it('TC_004: Verify that user should able to apply for Sr Software developer for London Location', () => {
+    page.navigateTo();
+    browser.sleep(2000);
+    page.click_SrSoftwareDevLondon();
+    browser.sleep(2000);
+    expect(page.getJobDescription_SrSoftwareDevLondon()).toEqual('London, UK');
+    page.click_ApplyNowButton();
+    browser.sleep(2000);
+    expect(page.verifyAttachResumePopUp()).toEqual('Attach Resume');
+  });
+
+  it('TC_005: Verify that user should able to apply for Testing Engineer for Gurgaon Location', () => {
+    page.navigateTo();
+    browser.sleep(2000);
+    page.click_TestingEngineerGurgaon();
+    browser.sleep(2000);
+    expect(page.getJobDescription_TestingEngGurgaon()).toEqual('Gurgaon, India');
+    page.click_ApplyNowButton();
+    browser.sleep(2000);
+    expect(page.verifyAttachResumePopUp()).toEqual('Attach Resume');
+  });
+
+  it('TC_006: Verify that user should able to apply for Testing Engineer for London Location', () => {
+    page.navigateTo();
+    browser.sleep(2000);
+    page.click_TestingEngineerLondon();
+    browser.sleep(2000);
+    expect(page.getJobDescription_TestingEngLondon()).toEqual('London, UK');
+    page.click_ApplyNowButton();
+    browser.sleep(2000);
+    expect(page.verifyAttachResumePopUp()).toEqual('Attach Resume');
+  });
+
+  it('TC_007: Verify that user should able to apply forSenior Web Designer for London Location', () => {
+    page.navigateTo();
+    browser.sleep(2000);
+    page.click_WebDesignerLondon();
+    browser.sleep(2000);
+    expect(page.getJobDescription_WebDesignerLondon()).toEqual('London, UK');
+    page.click_ApplyNowButton();
+    browser.sleep(2000);
+    expect(page.verifyAttachResumePopUp()).toEqual('Attach Resume');
+  });
+
+  it('TC_008: Verify that user should able to apply for Senior Web Designer for Gurgaon Location', () => {
+    page.navigateTo();
+    browser.sleep(2000);
+    page.click_WebDesignerGurgaon();
+    browser.sleep(2000);
+    expect(page.getJobDescription_WebDesignerGurgaon()).toEqual('Gurgaon, India');
+    page.click_ApplyNowButton();
+    browser.sleep(2000);
+    expect(page.verifyAttachResumePopUp()).toEqual('Attach Resume');
+
   });
 
   afterEach(async () => {
@@ -21,3 +97,4 @@ describe('workspace-project App', () => {
     } as logging.Entry));
   });
 });
+
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,92 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+    return element(by.xpath('//h1[contains(text(),"Browse Jobs")]')).getText() as Promise<string>;
   }
+
+
+  getJobLinkText_SrSoftwareDevGurgaon(): Promise<string> {
+    return element(by.xpath('//li[1]//a[1]//span[1]')).getText() as Promise<string>;
+  }
+
+  getJobLinkText_SrSoftwareDevLondon(): Promise<string> {
+    return element(by.xpath('//li[2]//a[1]//span[1]')).getText() as Promise<string>;
+  }
+
+  getJobLinkText_TestingEngGurgaon(): Promise<string> {
+    return element(by.xpath('//li[3]//a[1]//span[1]')).getText() as Promise<string>;
+  }
+
+  getJobLinkText_TestingEngLondon(): Promise<string> {
+    return element(by.xpath('//li[4]//a[1]//span[1]')).getText() as Promise<string>;
+  }
+
+  getJobLinkText_SrWebDesignerGurgaon(): Promise<string> {
+    return element(by.xpath('//li[5]//a[1]//span[1]')).getText() as Promise<string>;
+  }
+
+  getJobLinkText_SrWebDesignerLondon(): Promise<string> {
+    return element(by.xpath('//li[6]//a[1]//span[1]')).getText() as Promise<string>;
+  }
+
+  click_SrSoftwareDevGurgaon(): Promise<void>{
+    return element(by.xpath('//li[1]//a[1]//span[1]')).click() as Promise<void>;
+  }
+
+  click_SrSoftwareDevLondon(): Promise<void>{
+    return element(by.xpath('//li[2]//a[1]//span[1]')).click() as Promise<void>;
+  }
+
+  getJobDescription_SrSoftwareDevGurgaon(): Promise<string> {
+    return element(by.xpath('//p[contains(text(),"Gurgaon, India")]')).getText() as Promise<string>;
+
+  }
+
+  getJobDescription_SrSoftwareDevLondon(): Promise<string> {
+    return element(by.xpath('//p[contains(text(),"London, UK")]')).getText() as Promise<string>;
+  }
+
+  click_TestingEngineerGurgaon(): Promise<void>{
+    return element(by.xpath('//li[3]//a[1]//span[1]')).click() as Promise<void>;
+  }
+
+  getJobDescription_TestingEngGurgaon(): Promise<string> {
+    return element(by.xpath('//p[contains(text(),"Gurgaon, India")]')).getText() as Promise<string>;
+  }
+
+
+
+  click_TestingEngineerLondon(): Promise<void>{
+    return element(by.xpath('//li[4]//a[1]//span[1]')).click() as Promise<void>;
+  }
+
+  getJobDescription_TestingEngLondon(): Promise<string> {
+    return element(by.xpath('//p[contains(text(),"London, UK")]')).getText() as Promise<string>;
+  }
+
+  click_WebDesignerGurgaon(): Promise<void>{
+    return element(by.xpath('//li[6]//a[1]//span[1]')).click() as Promise<void>;
+  }
+
+  getJobDescription_WebDesignerLondon(): Promise<string> {
+    return element(by.xpath('//p[contains(text(),"London, UK")]')).getText() as Promise<string>;
+  }
+
+  click_WebDesignerLondon(): Promise<void>{
+    return element(by.xpath('//li[5]//a[1]//span[1]')).click() as Promise<void>;
+  }
+
+  getJobDescription_WebDesignerGurgaon(): Promise<string> {
+    return element(by.xpath('//p[contains(text(),"Gurgaon, India")]')).getText() as Promise<string>;
+  }
+
+  click_ApplyNowButton(): Promise<void>{
+    return element(by.xpath('//button[contains(text(),"APPLY NOW")]')).click() as Promise<void>;
+  }
+
+  verifyAttachResumePopUp(): Promise<string> {
+    return element(by.xpath('//label[contains(text(),"Attach Resume")]')).getText() as Promise<string>;
+  }
+
 }
+
